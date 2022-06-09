52ddac61261da73fa7fffeb0fe7a354097b033b0
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-06-09 13:39:31 +0200

    Add counter

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,15 +1,18 @@
+import Countdown from "react-countdown";
+
 function Intro(){
     return(
         <>
             <div className="intro">
                 <div className="introBackground">
-                    {/* <div className="introText">
-                        <p>Welkom bij Multimedia & Creatieve Technologieën van de Erashmushogeschool Brussel!</p>
+                    <div className="introText">
+                        {/* <p>Welkom bij Multimedia & Creatieve Technologieën van de Erashmushogeschool Brussel!</p>
                         <p>Klaar om onze richting te ontdekken?</p>
 
                         <input type="text" placeholder="Your name"/>
-                        <button>Let's go!</button>
-                    </div> */}
+                        <button>Let's go!</button> */}
+                    <Countdown date={new Date(2022,5,24,18,0,0,0)}/>
+                    </div>
                 </div>
             </div>
         </>
