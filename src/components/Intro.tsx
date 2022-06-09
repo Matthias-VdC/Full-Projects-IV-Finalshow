609a23fad460c25ad205a214e714db62bd33052c
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-06-09 11:57:52 +0200

    Create Intro component for later use

---
+++ src/components/Intro.tsx
@@ -0,0 +1,19 @@
+function Intro(){
+    return(
+        <>
+            <div className="intro">
+                <div className="introBackground">
+                    <div className="introText">
+                        <p>Welkom bij Multimedia & Creatieve Technologieën van de Erashmushogeschool Brussel!</p>
+                        <p>Klaar om onze richting te ontdekken?</p>
+
+                        <input type="text" placeholder="Your name"/>
+                        <button>Let's go!</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export {Intro}
