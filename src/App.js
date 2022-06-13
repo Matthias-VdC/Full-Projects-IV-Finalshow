0c83dd9c714ecb7495b45c04e78e7d7bdd430079
Ricardo <ricardo.mayorga9805@gmail.com>
2022-06-13 10:01:59 +0200

    Camera Animation works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Canvas, useThree } from "react-three-fiber";
-import { OrbitControls } from "@react-three/drei";
 import { Suspense, useEffect, useState } from "react";
 import * as THREE from "three";
 import gsap, { Power4 } from "gsap";
+import { OrbitControls } from "@react-three/drei";
 
 function Box({ onClick, position = [0, 0, 0], color = "red" }) {
     return (
@@ -19,30 +19,61 @@ function Plane() {
     return (
         <mesh position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
             <planeBufferGeometry attach={"geometry"} args={[100, 100]} />
-            <meshLambertMaterial attach={"material"} color="grey" />
+            <meshBasicMaterial wireframe attach={"material"} color="blue" />
         </mesh>
     );
 }
+
+// const CameraController = () => {
+//     const { camera, gl } = useThree();
+//     useEffect(() => {
+//         const controls = new OrbitControls(camera, gl.domElement);
+//         controls.minDistance = 3;
+//         controls.maxDistance = 20;
+//         return () => {
+//             controls.dispose();
+//         };
+//     }, [camera, gl]);
+//     return null;
+// };
 
 function Scene() {
     const { camera } = useThree();
     const [animation, setAnimation] = useState(false);
     const [lastObject, setLastObject] = useState(null);
 
+    const [controlsEnabled, setControlsEnabled] = useState(true);
+
     const animateCamera = (e) => {
         console.log(e);
-        const startRotation = new THREE.Euler().copy(camera.rotation);
-        camera.lookAt(e.object.position);
-        const endRotation = camera.rotation;
-        camera.rotation.copy(startRotation);
-        const startOrientation = camera.quaternion.clone().normalize();
-        const targetOrientation = e.object.quaternion.clone().normalize();
 
+        const rotationMatrix = new THREE.Matrix4();
+
+        rotationMatrix.lookAt(camera.position, e.object.position, e.object.up);
+        const startOrientation = camera.quaternion.clone().normalize();
+        const targetOrientation = new THREE.Quaternion().setFromRotationMatrix(
+            rotationMatrix
+        );
+
+        setControlsEnabled(false);
         const tl = gsap.timeline({ paused: true });
         if (lastObject) {
             if (lastObject.position !== e.object.position) {
-                camera.quaternion.rotateTowards(endRotation, 50);
                 tl.to(
+                    {},
+                    {
+                        duration: 1,
+                        onUpdate() {
+                            camera.quaternion.slerp(
+                                targetOrientation,
+                                this.progress()
+                            );
+                        },
+                        // onComplete() {
+                        //     controls.enabled = true;
+                        // },
+                    }
+                ).to(
                     camera.position,
                     {
                         x: 10,
@@ -53,6 +84,9 @@ function Scene() {
                         onUpdate: () => {
                             camera.lookAt(e.object.position);
                         },
+                        // onComplete() {
+                        //     controls.enabled = true;
+                        // },
                     },
                     1
                 );
@@ -68,6 +102,9 @@ function Scene() {
                 onUpdate: () => {
                     camera.lookAt(e.object.position);
                 },
+                // onComplete() {
+                //     controls.enabled = true;
+                // },
             });
         else
             tl.to(camera.position, {
@@ -79,6 +116,9 @@ function Scene() {
                 onUpdate: () => {
                     camera.lookAt(e.object.position);
                 },
+                // onComplete() {
+                //     controls.enabled = true;
+                // },
             });
         tl.play();
         setLastObject(e.object);
@@ -87,10 +127,19 @@ function Scene() {
 
     return (
         <>
+            <OrbitControls enableZoom={false} enabled={controlsEnabled} />
             <hemisphereLight intensity={0.5} />
             <directionalLight intensity={0.3} />
-            <Box onClick={animateCamera} />
-            <Box onClick={animateCamera} position={[0, 0, 10]} color="blue" />
+            <group>
+                <Box onClick={animateCamera} />
+                <Box
+                    onClick={animateCamera}
+                    position={[0, 0, 5]}
+                    color="blue"
+                />
+                <Plane />
+                <gridHelper args={[100, 100, "blue", "blue"]} />
+            </group>
         </>
     );
 }
