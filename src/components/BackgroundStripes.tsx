532f4e77161a25b60cb8b883a7a070ff713c8252
MatthiasVandeCasteele <matthias.van.de.casteele@student.ehb.be>
2022-06-13 15:07:45 +0200

    background + text/img positioning done
    DONE:
    + Background done according to given design.
    + Text and images positioned in the center of the page
    
    TODO:
    - A bit too dry. Add cassette cover tape as subtitle background.
    - Make responsive.

---
+++ src/components/BackgroundStripes.tsx
@@ -0,0 +1,11 @@
+export default function BackgroundStripes() {
+  return (
+    <div id="stripes-container">
+      <div id="pink"></div>
+      <div id="yellow"></div>
+      <div id="orange"></div>
+      <div id="red"></div>
+      <div id="blue"></div>
+    </div>
+  );
+}
---
+++ src/pages/Info.page.tsx
@@ -0,0 +1,55 @@
+import BackgroundStripes from "../components/BackgroundStripes";
+
+export default function Info() {
+  return (
+    <div id="info-container">
+      <BackgroundStripes></BackgroundStripes>
+      <div className="side-bar" id="left-bar"></div>
+      <div className="side-bar" id="right-bar"></div>
+      <h1>Info</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, laborum
+        fugiat dolore dolores consequuntur accusantium sed cum ipsum ratione
+        delectus non culpa repellat odit porro natus esse voluptatem illum
+        assumenda!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam dolore
+        asperiores, perspiciatis velit maiores excepturi voluptatem officia
+        ipsum quis rem? Totam ullam commodi dolore repellendus quod nam tempore
+        possimus temporibus. Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Est fuga sapiente exercitationem doloremque debitis obcaecati
+        corporis perferendis placeat quaerat? Fugit incidunt perferendis
+        assumenda earum praesentium consequuntur iusto magnam iste tempora.
+      </p>
+      <img src="../images/campus.webp" alt="campus" />
+      <div className="h2-container">
+        <h2>subtitle</h2>
+        <img className="h2-image" src="../images/cassette-0.png" alt="" />
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+        doloremque repellendus inventore, alias quas nisi nemo veniam quo
+        debitis reprehenderit velit, fuga non! Ea minima blanditiis, cumque
+        aliquam quod aspernatur.
+      </p>
+      <img src="../images/campus.webp" alt="campus" />
+      <div className="h2-container">
+        <h2>subtitle</h2>
+        <img className="h2-image" src="../images/cassette-0.png" alt="" />
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem odio
+        reprehenderit delectus? Assumenda incidunt aspernatur laboriosam,
+        asperiores, iure, nihil eos voluptate ullam qui corporis repellendus
+        ipsam! Inventore nisi ullam autem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+        praesentium qui veritatis, incidunt eaque perspiciatis, dolorem sit aut
+        quasi vel voluptates consectetur officia similique nobis voluptas natus
+        ut laborum eius?
+      </p>
+    </div>
+  );
+}
