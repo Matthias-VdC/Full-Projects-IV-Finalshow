245a6bcca96d598493e4ce1e4557dd5fe1631cb3
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-06-13 10:53:07 +0200

    Add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" />
-        <Route path="admin">
-          <Route path="hub" element={<p>hub</p>} />
-          <Route path="info" element={<p>info</p>} />
-          <Route path="live" element={<p>live</p>} />
-          <Route path="timetable" element={<p>timetable</p>} />
-          <Route path="showroom" element={<p>showroom</p>} />
-        </Route>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+          <Route path="admin">
+            <Route path="hub" element={<p>hub</p>} />
+            <Route path="info" element={<p>info</p>} />
+            <Route path="live" element={<p>live</p>} />
+            <Route path="timetable" element={<p>timetable</p>} />
+            <Route path="showroom" element={<p>showroom</p>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
