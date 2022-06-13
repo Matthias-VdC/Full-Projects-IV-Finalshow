fb802d5d763acfa60d65f54606e73af6adb19793
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-06-13 16:57:22 +0200

    Add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.scss";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Index } from "./pages/Index.page";
+import ReactGA from "react-ga";
+const TRACKING_ID = "UA-229998340-1"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index/>} />
-        <Route path="admin">
-          <Route path="hub" element={<p>hub</p>} />
-          <Route path="info" element={<p>info</p>} />
-          <Route path="live" element={<p>live</p>} />
-          <Route path="timetable" element={<p>timetable</p>} />
-          <Route path="showroom" element={<p>showroom</p>} />
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="admin">
+            <Route path="hub" element={<p>hub</p>} />
+            <Route path="info" element={<p>info</p>} />
+            <Route path="live" element={<p>live</p>} />
+            <Route path="timetable" element={<p>timetable</p>} />
+            <Route path="showroom" element={<p>showroom</p>} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
