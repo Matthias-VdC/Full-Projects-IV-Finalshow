f7b0347f6b4348cefab55778a6a0fbde5f84a86e
IlyesDjari <71768687+IlyesDjari@users.noreply.github.com>
2022-06-14 09:07:27 +0200

    WIP responsiveness on all devices

--- src/components/LiveYoutubeChat.tsx
+++ src/components/LiveYoutubeChat.tsx
@@ -5,7 +5,7 @@ function LiveYoutubeChat() {
 
     return (
     <div className="chat-container">
-    <iframe src="https://youtube.com/live_chat?v=5qap5aO4i9A&amp;embed_domain=10.3.208.84"></iframe>
+    <iframe src="https://youtube.com/live_chat?v=5qap5aO4i9A&amp;embed_domain=localhost"></iframe>
     </div>
     )
 }
