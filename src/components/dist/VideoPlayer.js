55a129df1dceafdcc30ac7a0917da9e972cd4045
IlyesDjari <71768687+IlyesDjari@users.noreply.github.com>
2022-06-14 14:00:22 +0200

    Fixed sass bugs

--- src/components/dist/VideoPlayer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function VideoPlayer() {
-    return (React.createElement("div", { className: "video-container" },
-        React.createElement("iframe", { width: "950", height: "534", src: "https://www.youtube.com/embed/5qap5aO4i9A", title: "YouTube video player", frameBorder: "0", allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture", allowFullScreen: true })));
-}
-exports["default"] = VideoPlayer;
-
-//# sourceMappingURL=VideoPlayer.js.map
