9ea9dd20ec78f05d2b8cc287a0402304dd05086d
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-06-14 10:26:53 +0200

    Update VHS components with multiple background and random rotation

--- src/components/VHS.tsx
+++ src/components/VHS.tsx
@@ -1,25 +1,55 @@
 import React from "react";
-import vhsYellow from "../assets/img/vhs-yellow.jpg";
+import vhsYellow from "../assets/img/yellow-vhs.png";
+import vhsRed from "../assets/img/red-vhs.png";
+import vhsBlue from "../assets/img/blue-vhs.png";
+import vhsOrange from "../assets/img/orange-vhs.png";
 import "../styles/components/_vhs.scss";
 
-export default function VHS() {
+interface IProps {
+  color: string;
+  subtitle?: string;
+  title: string;
+  startTime: string;
+  endTime: string;
+}
+
+export default function VHS(props: IProps) {
+  let bgImg: string = vhsYellow;
+  let css: string = `
+  .vhs-container {
+    transform: rotate(${Math.floor(Math.random() * (2 - -2) + -2)}deg)
+  }
+  `;
+
+  switch (props.color) {
+    case "yellow":
+      bgImg = vhsYellow;
+      break;
+    case "red":
+      bgImg = vhsRed;
+      break;
+    case "blue":
+      bgImg = vhsBlue;
+      break;
+    case "orange":
+      bgImg = vhsOrange;
+      break;
+    default:
+      bgImg = vhsYellow;
+      break;
+  }
   return (
     <div className="vhs-container">
       <div className="timing-container">
-        <p className="start-time">18:00:00</p>
-        <p className="end-time">18:30:00</p>
+        <p className="start-time">{props.startTime}</p>
+        <p className="end-time">{props.endTime}</p>
       </div>
       <div className="title-container">
-        <h2>Uitreiking</h2>
-        <h1>WEB & APP</h1>
+        <h2>{props.subtitle || ""}</h2>
+        <h1>{props.title}</h1>
       </div>
-      <img
-        src={vhsYellow}
-        alt=""
-        className="vhs-bg-image"
-        width="10"
-        height="1"
-      />
+      <img src={bgImg} alt="" className="vhs-bg-image" width="10" height="1" />
+      <style>{css}</style>
     </div>
   );
 }
