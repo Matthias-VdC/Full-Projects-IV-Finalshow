721846f81014ef46902786933d41a72b0a2bf21b
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-06-14 16:26:06 +0200

    Add hamburger menu

--- src/components/static/Header.tsx
+++ src/components/static/Header.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
+import { useSwipeable } from "react-swipeable";
 import logo from "../../assets/img/Final-Show-Logo.svg";
 import hamburgerIcon from "../../assets/img/menu-icon.svg";
 
@@ -8,10 +9,41 @@ export default function Header() {
   const location = useLocation();
   console.log(location);
 
+  const [isOpen, setOpen] = useState(false);
+
+  const handleIsOpen = () => {
+    setOpen(!isOpen);
+  };
+
+  const closeSideBar = () => {
+    setOpen(false);
+  };
+
   if (location.pathname != "/") {
     return (
       <>
         <header>
+          <Menu
+            customBurgerIcon={
+              <img src="https://static.thenounproject.com/png/696519-200.png" />
+            }
+            isOpen={isOpen}
+            onOpen={handleIsOpen}
+            onClose={handleIsOpen}
+          >
+            <Link onClick={closeSideBar} to="admin/info">
+              INFO
+            </Link>
+            <Link onClick={closeSideBar} to="admin/live">
+              LIVE SHOW
+            </Link>
+            <Link onClick={closeSideBar} to="admin/timetable">
+              TIMETABLE
+            </Link>
+            <Link onClick={closeSideBar} to="admin/showroom">
+              SHOWROOM{" "}
+            </Link>
+          </Menu>
           <div className="logo-container">
             <img src={logo} alt="logo" />
           </div>
