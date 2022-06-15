43876c4f2cbab2ae8fa37b99b36c0665ebf37193
MatthiasVandeCasteele <matthias.van.de.casteele@student.ehb.be>
2022-06-15 15:42:06 +0200

    Road added to scene + commodore

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -4,58 +4,60 @@ import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { ThreeEvent, useThree, Vector3 } from "react-three-fiber";
 import * as THREE from "three";
 import {
-    Ground,
-    InfoBlock,
-    LiveStreamBlock,
-    ShowRoomBlock,
-    TimeTableBlock,
-    Topography,
+  Ground,
+  InfoBlock,
+  LiveStreamBlock,
+  ShowRoomBlock,
+  TimeTableBlock,
+  Topography,
 } from "./models";
 import { Isopotamia } from "./models/Buildings/Isopotamia";
+import Commodore from "./models/Commodore";
 import { StreetLight } from "./models/Objects";
 
 const Box = () => {
-    return (
-        <mesh position={[0, 0, -400]}>
-            <boxBufferGeometry attach={"geometry"} args={[50, 50, 50]} />
-            <meshLambertMaterial attach={"material"} color={"white"} />
-        </mesh>
-    );
+  return (
+    <mesh position={[0, 0, 0]}>
+      <boxBufferGeometry attach={"geometry"} args={[5, 5, 5]} />
+      <meshLambertMaterial attach={"material"} color={"white"} />
+    </mesh>
+  );
 };
 
 type SceneProps = {
-    setPreview: Dispatch<SetStateAction<React.ReactNode>>;
-    setShowPreview: Dispatch<SetStateAction<boolean>>;
+  setPreview: Dispatch<SetStateAction<React.ReactNode>>;
+  setShowPreview: Dispatch<SetStateAction<boolean>>;
 };
 
 export const Scene = ({ setPreview, setShowPreview }: SceneProps) => {
-    const { camera } = useThree();
-    const [enableOrbit, setEnableOrbit] = useState(true);
-    const [currentTarget, setCurrentTarget] = useState<Vector3>([0, 0, 0]);
-    const [minDistance, setMinDistance] = useState(700);
-    const [hasAnimated, setHasAnimated] = useState(false);
+  const { camera } = useThree();
+  const [enableOrbit, setEnableOrbit] = useState(true);
+  const [currentTarget, setCurrentTarget] = useState<Vector3>([0, 0, 0]);
+  const [minDistance, setMinDistance] = useState(700);
+  const [hasAnimated, setHasAnimated] = useState(false);
 
-    return (
-        <>
-            <OrbitControls
-                // minDistance={minDistance}
-                enabled={enableOrbit}
-                enableZoom={false}
-                target={currentTarget}
-                minPolarAngle={-Math.PI / 3}
-                maxPolarAngle={Math.PI / 2}
-                minAzimuthAngle={-Math.PI / 3}
-                maxAzimuthAngle={Math.PI / 3}
-            />
-            <directionalLight intensity={0.4} />
-            <hemisphereLight intensity={1} />
-            <gridHelper
-                args={[100, 100, "blue", "blue"]}
-                onClick={(e) => console.log(e)}
-            />
-            {/* // <Isopotamia scale={20} /> */}
-            {/* <StreetLight scale={20} /> */}
-            <Topography rotation={[0, Math.PI, 0]} position={[200, 0, 300]} />
-        </>
-    );
+  return (
+    <>
+      <OrbitControls
+        // minDistance={minDistance}
+        enabled={enableOrbit}
+        enableZoom={true}
+        enablePan={true}
+        target={currentTarget}
+        minPolarAngle={-Math.PI / 2}
+        maxPolarAngle={Math.PI / 2}
+        minAzimuthAngle={-Math.PI / 2}
+        maxAzimuthAngle={Math.PI / 2}
+      />
+      <directionalLight intensity={0.4} />
+      <hemisphereLight intensity={1} />
+      {/* <gridHelper
+        args={[100, 100, "blue", "blue"]}
+        onClick={(e) => console.log(e)}
+      /> */}
+      {/* // <Isopotamia scale={20} /> */}
+      {/* <StreetLight scale={20} /> */}
+      <Topography rotation={[0, Math.PI, 0]} position={[200, 0, 300]} />
+    </>
+  );
 };
--- src/components/models/Topography.tsx
+++ src/components/models/Topography.tsx
@@ -1,27 +1,31 @@
 import React, { useRef } from "react";
 import { Parcel } from ".";
+import Commodore from "./Commodore";
 import { Ground } from "./Ground";
+import { Roads } from "./Roads";
 
 export const Topography = ({ ...props }: JSX.IntrinsicElements["group"]) => {
-    const ref = useRef<THREE.Group>(null);
-    return (
-        //[400, 20, 900]
-        <group ref={ref} {...props}>
-            <Parcel position={[50, 0, 50]} /> {/** 1 */}
-            <Parcel position={[200, 0, 50]} /> {/** 2 */}
-            <Parcel position={[350, 0, 50]} /> {/** 3 */}
-            <Parcel position={[50, 0, 200]} /> {/** 4 */}
-            <Parcel position={[275, 0, 200]} scale={[2.5, 2, 1]} /> {/** 5 */}
-            <Parcel position={[50, 0, 350]} /> {/** 7 */}
-            <Parcel position={[350, 0, 425]} scale={[1, 2, 2.5]} /> {/** 9 */}
-            <Parcel position={[50, 0, 500]} /> {/** 10 */}
-            <Parcel position={[200, 0, 500]} scale={[1, 0.5, 4]} />
-            <Parcel position={[350, 0, 500]} /> {/** 10 */}
-            {/** 11 */}
-            <Parcel position={[125, 0, 800]} scale={[2.5, 2, 1]} /> {/** 14 */}
-            <Parcel position={[50, 0, 725]} scale={[1, 2, 2.5]} /> {/** 14 */}
-            <Parcel position={[350, 0, 725]} scale={[1, 2, 2.5]} /> {/** 16 */}
-            <Ground />
-        </group>
-    );
+  const ref = useRef<THREE.Group>(null);
+  return (
+    //[400, 20, 900]
+    <group ref={ref} {...props}>
+      <Commodore scale={20} position={[75, 50, 0]} />
+      <Parcel position={[50, 0, 50]} /> {/** 1 */}
+      <Roads></Roads>
+      <Parcel position={[200, 0, 50]} /> {/** 2 */}
+      <Parcel position={[350, 0, 50]} /> {/** 3 */}
+      <Parcel position={[50, 0, 200]} /> {/** 4 */}
+      <Parcel position={[275, 0, 200]} scale={[2.5, 2, 1]} /> {/** 5 */}
+      <Parcel position={[50, 0, 350]} /> {/** 7 */}
+      <Parcel position={[350, 0, 425]} scale={[1, 2, 2.5]} /> {/** 9 */}
+      <Parcel position={[50, 0, 500]} /> {/** 10 */}
+      <Parcel position={[200, 0, 500]} scale={[1, 0.5, 4]} />
+      <Parcel position={[350, 0, 500]} /> {/** 10 */}
+      {/** 11 */}
+      <Parcel position={[125, 0, 800]} scale={[2.5, 2, 1]} /> {/** 14 */}
+      <Parcel position={[50, 0, 725]} scale={[1, 2, 2.5]} /> {/** 14 */}
+      <Parcel position={[350, 0, 725]} scale={[1, 2, 2.5]} /> {/** 16 */}
+      {/* <Ground /> */}
+    </group>
+  );
 };
