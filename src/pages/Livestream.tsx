38b431c2efe6d275fc3659fa0c95307af04ee909
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-06-15 15:57:36 +0200

    Fix hamburger menu

--- src/pages/Livestream.tsx
+++ src/pages/Livestream.tsx
@@ -1,31 +1,26 @@
 import BackArrow from "../components/BackArrow";
 import LiveYoutubeChat from "../components/LiveYoutubeChat";
 import Share from "../components/Share";
-import Subscribe from "../components/Subscribe";
 import VideoPlayer from "../components/VideoPlayer";
 
-
-
 function Livestream() {
-    
-
-
-    return (<>
-    <div className="livebody">
-    <BackArrow></BackArrow>
-    <div className="streaming">
-    <VideoPlayer></VideoPlayer>
-    <div className="streamingBottom">
-        <h1>EhB Final show 2022</h1>
-        <div className="actionsYoutube">
-            <Share></Share>
+  return (
+    <>
+      <div className="livebody">
+        <BackArrow></BackArrow>
+        <div className="streaming">
+          <VideoPlayer></VideoPlayer>
+          <div className="streamingBottom">
+            <h1>EhB Final show 2022</h1>
+            <div className="actionsYoutube">
+              <Share></Share>
+            </div>
+          </div>
+          <LiveYoutubeChat></LiveYoutubeChat>
         </div>
-    </div>
-    <LiveYoutubeChat></LiveYoutubeChat>
-    </div>
-    </div>
-
-    </>);
+      </div>
+    </>
+  );
 }
 
 export default Livestream;
