01ecfa92dd48e33c6e328114d68d2511c2cd5607
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-06-15 10:38:25 +0200

    Info in background stripes

--- src/components/BackgroundStripes.tsx
+++ src/components/BackgroundStripes.tsx
@@ -1,11 +1,22 @@
 export default function BackgroundStripes() {
   return (
     <div id="stripes-container">
-      <div id="pink"></div>
-      <div id="yellow"></div>
-      <div id="orange"></div>
-      <div id="red"></div>
-      <div id="blue"></div>
+      <div id="pink" className="stripe">
+        <p>Multimedia &</p>
+        <p>Creatieve Technologie</p>
+      </div>
+      <div id="yellow" className="stripe">
+        
+      </div>
+      <div id="orange" className="stripe">
+        <p>Livestream</p>
+        <p>24/06 19u</p>
+      </div>
+      <div id="red" className="stripe">
+        <p>Infodag</p>
+        <p>25/06 10u</p>
+      </div>
+      <div id="blue" className="stripe"></div>
     </div>
   );
 }
