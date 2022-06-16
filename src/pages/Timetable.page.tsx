9b8b10b0eb8c9a0e2fbc4a0c5e18c41ca9296508
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-06-16 13:38:09 +0200

    Fix timetable times

--- src/pages/Timetable.page.tsx
+++ src/pages/Timetable.page.tsx
@@ -8,14 +8,14 @@ export default function Timetable() {
         <h1 className="title">Live Show Timetable</h1>
         <VHS color="pink" variation={1} title="intro" startTime="19:00" />
         <VHS color="white" variation={1} />
-        <VHS color="red" variation={4} title="WEB & APP" startTime="19:03" />
+        <VHS color="red" variation={4} title="WEB & APP" startTime="19:01" />
         <VHS color="red" variation={1} title="uitreiking" rotate />
         <VHS color="white" variation={1} rotate />
         <VHS
           color="blue"
           variation={2}
           title="SMART TECHNOLOGY"
-          startTime="19:07"
+          startTime="19:06"
         />
         <VHS color="blue" variation={3} title="uitreiking" rotate />
         <VHS color="white" variation={1} />
@@ -23,46 +23,46 @@ export default function Timetable() {
           color="pink"
           variation={1}
           title="spel 1: spill your guts"
-          startTime="19:12"
+          startTime="19:11"
         />
         <VHS color="white" variation={1} rotate />
-        <VHS color="orange" variation={1} title="MOTION" startTime="19:18" />
+        <VHS color="orange" variation={1} title="MOTION" startTime="19:17" />
         <VHS color="orange" variation={2} title="uitreiking" rotate />
         <VHS color="white" variation={1} rotate />
         <VHS
           color="yellow"
           variation={3}
           title="EXTENDED REALITY"
-          startTime="19:23"
+          startTime="19:22"
         />
         <VHS color="yellow" variation={4} title="uitreiking" rotate />
         <VHS color="white" variation={1} />
         <VHS
           color="pink"
           variation={1}
-          title="spel 2: lorem ipsum"
-          startTime="19:25"
+          title="spel 2: spill your guts"
+          startTime="19:27"
         />
         <VHS color="white" variation={1} rotate />
         <VHS
           color="pink"
           variation={1}
           title="HOOFDPRIJS"
-          startTime="19:28"
+          startTime="19:32"
           rotate
         />
         <VHS
           color="pink"
           variation={1}
           title="YOUNG POTENTIAL"
-          startTime="19:34"
+          startTime="19:35"
         />
         <VHS color="white" variation={1} rotate />
         <VHS
           color="pink"
           variation={1}
           title="outro"
-          startTime="19:37"
+          startTime="19:38"
           rotate
         />
       </div>
