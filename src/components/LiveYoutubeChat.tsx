a36c19a67a43c0c788afb395a8e64529975a6800
IlyesDjari <71768687+IlyesDjari@users.noreply.github.com>
2022-06-16 11:32:09 +0200

    Portrait mode, more white space

--- src/components/LiveYoutubeChat.tsx
+++ src/components/LiveYoutubeChat.tsx
@@ -2,7 +2,7 @@ function LiveYoutubeChat() {
 
     return (
     <div className="chat-container">
-    <iframe src="https://youtube.com/live_chat?v=5qap5aO4i9A&amp;embed_domain=localhost"></iframe>
+    <iframe src="https://youtube.com/live_chat?v=5qap5aO4i9A&amp;embed_domain=10.3.208.30"></iframe>
     </div>
     )
 }
