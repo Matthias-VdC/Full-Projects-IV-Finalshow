0af69a78b1f81d5cd1c0ec8ab69b384929fb8785
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-06-16 14:15:35 +0200

    Hover animation

--- src/components/BackgroundStripes.tsx
+++ src/components/BackgroundStripes.tsx
@@ -1,18 +1,19 @@
 export default function BackgroundStripes() {
   return (
     <div id="stripes-container">
-      <a href="#info-container" id="pink" className="stripe">
+      <a href="#info-container" id="pink" className="stripe stripe-hover">
         <p>Multimedia &</p>
         <p>Creatieve Technologie</p>
       </a>
-      <a id="yellow" className="stripe">
+      <a id="yellow" className="stripe stripe-hover">
         
       </a>
-      <a href="#livestreamTitle" id="orange" className="stripe">
+      <a href="#livestreamTitle" id="orange" className="stripe stripe-hover">
         <p>Livestream</p>
         <p>24/06 19u</p>
       </a>
-      <a href="#infoDagSection" id="red" className="stripe">
+      <a href="#infoDagSection" id="red" className="stripe stripe-hover">
+
         <p>Infodag</p>
         <p>25/06 10u</p>
       </a>
