4ece24b9e45bd923271c7be4b8754044051b1626
Ricardo <ricardo.mayorga9805@gmail.com>
2022-06-16 11:38:53 +0200

    Merged origin/development

--- src/pages/Index.page.tsx
+++ src/pages/Index.page.tsx
@@ -5,7 +5,6 @@ import { Television } from "../components/Television";
 function Index() {
     return (
         <div className="homepage">
-<<<<<<< HEAD
             <div className="homepageBackground">
                 <div className="upperBackground">
                     <Logo />
@@ -13,9 +12,7 @@ function Index() {
                 </div>
                 <div className="lowerBackground"></div>
             </div>
-=======
             <Television></Television>
->>>>>>> 049b1a9add50909bf40d15af21fba51bfea0ff3c
         </div>
     );
 }
