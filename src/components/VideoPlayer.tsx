2fa787a903bdcfaa33e825b536230f43098cfd82
IlyesDjari <71768687+IlyesDjari@users.noreply.github.com>
2022-06-20 16:32:44 +0200

    Commented the loader out for this branch

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -13,7 +13,7 @@ function VideoPlayer() {
     return  (
         <>
     <div className="video-container">
-        {loader ? (<Loader />) : null}
+        {/*loader ? (<Loader />) : null*/}
                 <iframe width="950" height="534" loading='lazy' onLoad={isLoaded} src="https://www.youtube.com/embed/ZwIB1XPI64w" title="Behind the scenes" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
                 </iframe>
             </div>
