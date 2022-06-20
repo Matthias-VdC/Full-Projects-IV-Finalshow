892b3ab57b788eb0376cee3565eb24a189882dee
IlyesDjari <71768687+IlyesDjari@users.noreply.github.com>
2022-06-20 15:54:08 +0200

    Used React player instead of iFrame

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,23 +1,30 @@
 import Loader from "../components/Loader";
-import {useState} from 'react';
-
-/* This is a React component that is using the useState hook to set the state of the loader to true.
-When the iframe is loaded, the state of the loader is set to false. */
+import { useState } from 'react';
+import ReactPlayer from "react-player";
 
 function VideoPlayer() {
     const [loader, setLoader] = useState(true);
-
-    const isLoaded =  () => {
+    const isLoaded = () => {
         setLoader(false);
     }
-    return  (
+
+    /* Returning the video player. */
+    return (
         <>
-    <div className="video-container">
-        {loader ? (<Loader />) : null}
-                <iframe width="950" height="534" loading='lazy' onLoad={isLoaded} src="https://www.youtube.com/embed/ZwIB1XPI64w" title="Behind the scenes" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
-                </iframe>
+            <div className="video-container">
+                {loader ? (<Loader />) : null}
+                <ReactPlayer
+                    url={"https://www.youtube.com/embed/ZwIB1XPI64w"}
+                    width="950"
+                    height="534"
+                    playing={true}
+                    onReady={isLoaded}
+                    muted={false}
+                    controls={true}
+                    className="reactPlayer"
+                ></ReactPlayer>
             </div>
-            </>
+        </>
     )
 }
 
