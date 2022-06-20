0a7d03f5ec4bb2be42e8bf1fe2f8a2bcca057106
IlyesDjari <71768687+IlyesDjari@users.noreply.github.com>
2022-06-20 14:02:11 +0200

    Added title to videoPlayer

--- src/components/LiveYoutubeChat.tsx
+++ src/components/LiveYoutubeChat.tsx
@@ -5,7 +5,7 @@
 function LiveYoutubeChat() {
     return (
     <div className="chat-container">
-    <iframe src="https://youtube.com/live_chat?v=ZwIB1XPI64w&amp;embed_domain=finalshow.be"></iframe>
+    <iframe title="videoPlayer" src="https://youtube.com/live_chat?v=ZwIB1XPI64w&amp;embed_domain=finalshow.be"></iframe>
     </div>
     )
 }
