0074b3ed85c7246627f3c3534ae4642829ce2d71
IlyesDjari <71768687+IlyesDjari@users.noreply.github.com>
2022-06-20 13:57:34 +0200

    Loading logic works, waiting for animation

--- src/components/LiveYoutubeChat.tsx
+++ src/components/LiveYoutubeChat.tsx
@@ -1,5 +1,8 @@
+/**
+ * It returns a div with an iframe inside it
+ * @returns A div with an iframe inside.
+ */
 function LiveYoutubeChat() {
-
     return (
     <div className="chat-container">
     <iframe src="https://youtube.com/live_chat?v=ZwIB1XPI64w&amp;embed_domain=finalshow.be"></iframe>
