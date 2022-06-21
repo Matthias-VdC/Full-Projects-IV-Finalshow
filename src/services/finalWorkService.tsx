d6be54494c29c3ad23c26f1d3d5f33534755c6bd
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-06-21 16:43:20 +0200

    Add heroku middelware server

--- src/services/finalWorkService.tsx
+++ src/services/finalWorkService.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 
 class FinalWorkService {
+  private readonly url =
+    "https://mighty-earth-63459.herokuapp.com/http://finalshowcasebackend.be/finalwork";
 
-    private readonly url = 'http://finalshowcasebackend.be/finalwork'
+  public async fetchFinalWorks() {
+    const response = await fetch(`${this.url}`);
 
-    public async fetchFinalWorks(){
-        const response = await fetch(`${this.url}`);
-
-        return response.json();
-    }
-
+    return response.json();
+  }
 }
 
-
-export const finalWorkService = new FinalWorkService 
+export const finalWorkService = new FinalWorkService();
