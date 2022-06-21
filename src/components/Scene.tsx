89822484e08651be458f8834ce8f02bc5cd5ba0d
MatthiasVandeCasteele <matthias.van.de.casteele@student.ehb.be>
2022-06-21 16:43:39 +0200

    pacman added to arcade + events to arcade (WIP)

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,50 +1,59 @@
 import { OrbitControls, PresentationControls } from "@react-three/drei";
 import gsap, { Power3, Power4 } from "gsap/all";
-import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { ThreeEvent, useThree } from "react-three-fiber";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { ThreeEvent, useFrame, useThree } from "react-three-fiber";
 import * as THREE from "three";
 import { MathUtils, MeshLambertMaterial, Vector3 } from "three";
 import { Lights } from "./models/Lights";
 import { Room802 } from "./models/Room802";
 
 const Box = () => {
-    return (
-        <mesh position={[0, 0, 0]}>
-            <boxBufferGeometry attach={"geometry"} args={[5, 5, 5]} />
-            <meshLambertMaterial attach={"material"} color={"white"} />
-        </mesh>
-    );
+  return (
+    <mesh position={[0, 0, 0]}>
+      <boxBufferGeometry attach={"geometry"} args={[5, 5, 5]} />
+      <meshLambertMaterial attach={"material"} color={"white"} />
+    </mesh>
+  );
 };
 
 export const Scene = () => {
-    const { camera } = useThree();
-    const [enableOrbit, setEnableOrbit] = useState(true);
-    const [minDistance, setMinDistance] = useState(700);
-    const [hasAnimated, setHasAnimated] = useState(false);
+  const { camera, controls } = useThree();
+  const [enableOrbit, setEnableOrbit] = useState(true);
+  const [minDistance, setMinDistance] = useState(700);
+  const [hasAnimated, setHasAnimated] = useState(false);
 
-    return (
-        <>
-            <OrbitControls
-                // minDistance={minDistance}
-                enabled={enableOrbit}
-                enableZoom={true}
-                target={[0, 0, 0]}
-                // minPolarAngle={-Math.PI / 3}
-                // maxPolarAngle={Math.PI / 2}
-                // minAzimuthAngle={-Math.PI / 3}
-                // maxAzimuthAngle={Math.PI / 3}
-            />
-            <Room802
-                castShadow
-                receiveShadow
-                onClick={(e) => {
-                    const position = new Vector3();
-                    e.object.getWorldPosition(position);
-                    console.log(e.object, position);
-                }}
-                rotation={[0, MathUtils.degToRad(54), 0]}
-            />
-            <Lights />
-        </>
-    );
+  console.log(controls);
+
+  return (
+    <>
+      {/* <OrbitControls
+        // minDistance={minDistance}
+        enabled={enableOrbit}
+        enableZoom={true}
+        target={[0, 0, 0]}
+        makeDefault
+        // minPolarAngle={-Math.PI / 3}
+        // maxPolarAngle={Math.PI / 2}
+        // minAzimuthAngle={-Math.PI / 3}
+        // maxAzimuthAngle={Math.PI / 3}
+      /> */}
+      <Room802
+        castShadow
+        receiveShadow
+        onClick={(e) => {
+          const position = new Vector3();
+          e.object.getWorldPosition(position);
+        }}
+        rotation={[0, MathUtils.degToRad(54), 0]}
+      />
+      {/* <ambientLight intensity={0.5} /> */}
+      <Lights />
+    </>
+  );
 };
--- src/components/models/Lights.tsx
+++ src/components/models/Lights.tsx
@@ -9,53 +9,53 @@ import { SpotLightHelper } from "three";
 type Props = {};
 
 function MovingSpot({ vec = new THREE.Vector3(), ...props }) {
-    const light = useRef<THREE.SpotLight>(null);
-    const viewport = useThree((state) => state.viewport);
-    useHelper(light, SpotLightHelper, "Green");
-    useFrame((state) => {
-        light.current?.target.position.lerp(
-            vec.set(
-                (state.mouse.x * viewport.width) / 2,
-                (state.mouse.y * viewport.height) / 2,
-                0
-            ),
-            0.1
-        );
-        light.current?.target.updateMatrixWorld();
-    });
-    return (
-        <SpotLight
-            castShadow
-            ref={light}
-            penumbra={1}
-            distance={2000}
-            angle={0.35}
-            attenuation={5}
-            intensity={2}
-            {...props}
-        />
+  const light = useRef<THREE.SpotLight>(null);
+  const viewport = useThree((state) => state.viewport);
+  useHelper(light, SpotLightHelper, "Green");
+  useFrame((state) => {
+    light.current?.target.position.lerp(
+      vec.set(
+        (state.mouse.x * viewport.width) / 2,
+        (state.mouse.y * viewport.height) / 2,
+        0
+      ),
+      0.1
     );
+    light.current?.target.updateMatrixWorld();
+  });
+  return (
+    <SpotLight
+      castShadow
+      ref={light}
+      penumbra={1}
+      distance={2000}
+      angle={0.35}
+      attenuation={5}
+      intensity={2}
+      {...props}
+    />
+  );
 }
 export const Lights = (props: Props) => {
-    const spot = useRef(null);
-    const point = useRef(null);
-    const tvRect = useRef(null);
-    const arcade = useRef(null);
+  const spot = useRef(null);
+  const point = useRef(null);
+  const tvRect = useRef(null);
+  const arcade = useRef(null);
 
-    useHelper(spot, THREE.SpotLightHelper, "cyan");
-    useHelper(point, THREE.PointLightHelper, 50, "red");
-    useHelper(tvRect, RectAreaLightHelper, "pink");
-    useHelper(arcade, RectAreaLightHelper, "blue");
-    const depthBuffer = useDepthBuffer({ size: 256, frames: 1 });
+  useHelper(spot, THREE.SpotLightHelper, "cyan");
+  useHelper(point, THREE.PointLightHelper, 50, "red");
+  useHelper(tvRect, RectAreaLightHelper, "pink");
+  useHelper(arcade, RectAreaLightHelper, "blue");
+  const depthBuffer = useDepthBuffer({ size: 256, frames: 1 });
 
-    return (
-        <group>
-            <MovingSpot
-                depthBuffer={depthBuffer}
-                position={[600, 300, 600]}
-                color={"purple"}
-            />
-            {/* <CustomSpotLight
+  return (
+    <group>
+      <MovingSpot
+        depthBuffer={depthBuffer}
+        position={[600, 300, 600]}
+        color={"purple"}
+      />
+      {/* <CustomSpotLight
                 lightPosition={[300, 1000, 300]}
                 targetPosition={[0, 40, 0]}
                 distance={1500}
@@ -63,26 +63,26 @@ export const Lights = (props: Props) => {
                 penumbra={1}
                 ref={spot}
             /> */}
-            <pointLight
-                ref={point}
-                intensity={0.1}
-                color={"lightyellow"}
-                position={[-120, 185, -419]}
-            />
-            {/* <rectAreaLight
+      <pointLight
+        ref={point}
+        intensity={0.1}
+        color={"lightyellow"}
+        position={[-120, 185, -419]}
+      />
+      {/* <rectAreaLight
                 width={100}
                 height={100}
                 ref={arcade}
                 intensity={5}
                 rotation={[0, -Math.PI / 2, 0]}
             /> */}
-            <rectAreaLight
-                width={72}
-                height={50}
-                ref={tvRect}
-                intensity={10}
-                position={[152, 92, 3]}
-            />
-        </group>
-    );
+      <rectAreaLight
+        width={72}
+        height={50}
+        ref={tvRect}
+        intensity={10}
+        position={[152, 92, 3]}
+      />
+    </group>
+  );
 };
