aa92f12cdb56b91933151b73997c40f4b5e1ba61
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-06-22 17:57:17 +0200

    Fix merge conflict in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,32 +27,31 @@ const TRACKING_ID = "UA-229998340-1"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
 function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-        width,
-        height,
-    };
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
 }
 
 function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(
-        getWindowDimensions()
-    );
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
 
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
 
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    return windowDimensions;
+  return windowDimensions;
 }
 
 function App() {
-<<<<<<< HEAD
   const location = useLocation();
   const { height, width } = useWindowDimensions();
   useEffect(() => {
@@ -67,55 +66,19 @@ function App() {
           <Route path="info" element={<Info />} />
           <Route path="timetable" element={<Timetable2 />} />
           <Route path="live" element={<Livestream />} />
+          <Route path="hub" element={<Map />} />
           <Route path="showroom">
             <Route index element={<Home />} />
-            <Route path="finalwork/:id" element={<SharedDetail/>}/>
             <Route path="results" element={<Results />} />
             <Route path="detail" element={<DetailMob />} />
             <Route path="genomineerden" element={<Genomineerd />} />
             <Route path="winnaars" element={<Winnaar />} />
-          </Route>
-          <Route path="admin">
-            <Route path="hub" element={<p>hub</p>} />
           </Route>
           <Route path="*" element={<Notfound />}></Route>
         </Routes>
       </div>
     </div>
   );
-=======
-    const location = useLocation();
-    const { height, width } = useWindowDimensions();
-    useEffect(() => {
-        ReactGA.pageview(window.location.pathname + window.location.search);
-    }, []);
-    return (
-        <div className="App" id="app-container">
-            {location.pathname === "/timetable" && width < 1075 ? (
-                <></>
-            ) : (
-                <Header />
-            )}
-            <div id="page-container">
-                <Routes>
-                    <Route path="/" element={<Index />} />
-                    <Route path="info" element={<Info />} />
-                    <Route path="timetable" element={<Timetable2 />} />
-                    <Route path="live" element={<Livestream />} />
-                    <Route path="hub" element={<Map />} />
-                    <Route path="showroom">
-                        <Route index element={<Home />} />
-                        <Route path="results" element={<Results />} />
-                        <Route path="detail" element={<DetailMob />} />
-                        <Route path="genomineerden" element={<Genomineerd />} />
-                        <Route path="winnaars" element={<Winnaar />} />
-                    </Route>
-                    <Route path="*" element={<Notfound />}></Route>
-                </Routes>
-            </div>
-        </div>
-    );
->>>>>>> development
 }
 
 export default App;
